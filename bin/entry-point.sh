e4406936a98ab8dc3a342fec37e94f793af66744
Giampiero Gabbiani <giampiero@gabbiani.org>
2026-04-06 03:24:33 +0200

    Remove verbose flag from make-picture.py command in entry-point script

--- bin/entry-point.sh
+++ bin/entry-point.sh
@@ -135,7 +135,7 @@ PIC_FILE=$(basename "$PIC_PATH")
 
 echo "RENDER=$RENDER"
 
-xvfb-run -d $APP/make-picture.py -v 4 $RESOLUTION $CAMERA $PROJECTION $VIEW_AXES "$SCRIPT" $RENDER "$PIC_PATH"
+xvfb-run -d $APP/make-picture.py $RESOLUTION $CAMERA $PROJECTION $VIEW_AXES "$SCRIPT" $RENDER "$PIC_PATH"
 magick "$PIC_DIR/unscaled-$PIC_FILE" $RESIZE "$PIC_PATH"
 rm "$PIC_DIR/unscaled-$PIC_FILE"
 
