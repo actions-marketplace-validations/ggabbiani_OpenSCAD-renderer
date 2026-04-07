cfda26833facc882f3f314852a80f2d8d1247fcb
Giampiero Gabbiani <giampiero@gabbiani.org>
2026-04-07 18:16:51 +0200

    updated

--- bin/bump.py
+++ bin/bump.py
@@ -80,7 +80,7 @@ def git_chk():
         raise RuntimeError(f'Unclear git status:\n\n{out.stdout}')
     out = subprocess.run(['git', 'status', '-uno'], capture_output=True, text=True)
     if out.stdout.count('\n')>0:
-        raise RuntimeError(f'Islocal repo is behind?!:\n\n{out.stdout}')
+        raise RuntimeError(f'Unclear git status:\n\n{out.stdout}')
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Bump version on remote git origin.')
