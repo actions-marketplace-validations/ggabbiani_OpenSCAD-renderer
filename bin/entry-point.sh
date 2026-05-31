7cab33b6fdf1d0433cd521f5fdf305b4152708d8
Giampiero Gabbiani <giampiero@gabbiani.org>
2026-05-31 20:33:48 +0200

    Update .gitignore, action.yaml, and entry-point.sh to support color scheme input; add README.md for app directory

--- bin/entry-point.sh
+++ bin/entry-point.sh
@@ -19,10 +19,11 @@ trap 'on_exit $? $test' EXIT
 help() {
 cat <<EoH
 
-$(basename $0) [-?|-h|--help] [-c|--camera <position>] [-p|--projection <projection>] [-r|--resolution <resolution>] [-i|--image <image path>] [--render] SCRIPT
+$(basename $0) [-?|-h|--help] [-c|--camera <position>] [--colorscheme <scheme>] [-p|--projection <projection>] [-r|--resolution <resolution>] [-i|--image <image path>] [--render] SCRIPT
 
   -?|-h|--help      this help
   -c|--camera       OpenSCAD camera position
+  --colorscheme     OpenSCAD color scheme
   -i|--image        image path
   -p|--projection   'ortho' or 'perspective'
   -r|--resolution   target resolution in 'openscad' format i.e. 800x600
@@ -61,6 +62,12 @@ while (( "$#" )); do
     -c|--camera)
       if [ -n "$2" ]; then
         CAMERA="--camera=$2"
+      fi
+      shift 2
+      ;;
+    --colorscheme)
+      if [ -n "$2" ]; then
+        COLORSCHEME="--colorscheme=$2"
       fi
       shift 2
       ;;
@@ -135,7 +142,7 @@ PIC_FILE=$(basename "$PIC_PATH")
 
 echo "RENDER=$RENDER"
 
-xvfb-run -d $APP/make-picture.py $RESOLUTION $CAMERA $PROJECTION $VIEW_AXES "$SCRIPT" $RENDER "$PIC_PATH"
+xvfb-run -d $APP/make-picture.py $RESOLUTION $CAMERA $PROJECTION $VIEW_AXES "$COLORSCHEME" "$SCRIPT" $RENDER "$PIC_PATH"
 magick "$PIC_DIR/unscaled-$PIC_FILE" $RESIZE "$PIC_PATH"
 rm "$PIC_DIR/unscaled-$PIC_FILE"
 
---
+++ docker/renderer/app/README.md
@@ -0,0 +1,1 @@
+This directory contents are injected at build-time, so ignored from git.
